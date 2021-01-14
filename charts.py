2e915373946599c55ea67df07290ca4ce1c3e840
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-01-14 13:50:34 +0200

    Details

--- charts.py
+++ charts.py
@@ -26,19 +26,46 @@ def main():
     sorted_days = sorted(days.items())
     sorted_versions = sorted(versions)
 
-    rows = [["Day"]]
+    chart_rows = [["Day"]]
     for version in sorted_versions:
-        rows[len(rows) - 1].append(version[-7:])
+        chart_rows[len(chart_rows) - 1].append(version[-7:])
 
     for day in sorted_days:
-        rows.append([day[0]])
+        chart_rows.append([day[0]])
         for version in sorted_versions:
             if version in day[1]:
-                rows[len(rows) - 1].append((day[1][version]))
+                chart_rows[len(chart_rows) - 1].append((day[1][version]))
             else:
-                rows[len(rows) - 1].append(0)
+                chart_rows[len(chart_rows) - 1].append(0)
 
-    data = data.replace("{ data }", "%r" % rows)
+    report = {}
+    for version in sorted(sorted_versions, reverse=True):
+        report[version] = {}
+        for day in sorted_days:
+            report[version][day[0]] = 0
+            if version in day[1]:
+                report[version][day[0]] = day[1][version]
+
+    version_head = "<tr><th>Version</th>"
+
+    for day in sorted_days:
+        version_head += "<th>%s</th>" % day[0]
+
+    version_head += "</tr>"
+    version_body = ""
+
+    for row in report:
+        version_body += (
+            "<tr><td><a href='https://github.com/excalidraw/excalidraw/commit/%s'>%s</a></td>"
+            % (row[-7:], row)
+        )
+        for day in report[row]:
+            version_body += "<td>%s</td>" % (report[row][day] or "-")
+        version_body += "</tr>\n"
+
+    data = data.replace("{ data }", "%r" % chart_rows)
+    data = data.replace("{ version_head }", version_head)
+    data = data.replace("{ version_body }", version_body)
 
     with open(INDEX_FILE, "w") as index:
         index.write(data)
