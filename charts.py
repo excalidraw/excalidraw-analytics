6ec777f67d82ba5b4b17cc3fe7a823fdf76eca84
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-01-20 00:45:15 +0200

    Days of week

--- charts.py
+++ charts.py
@@ -1,4 +1,3 @@
-from datetime import date
 from datetime import datetime
 from datetime import timedelta
 from opencolor import oc
@@ -46,7 +45,7 @@ def parse_day(filename):
     filename = filename.replace(".json", "")
     file_date = datetime.strptime(filename, "%Y-%m-%d")
     today = datetime.today()
-    return file_date > today + timedelta(days=-MAX_DAYS)
+    return file_date > today + timedelta(days=-MAX_DAYS-1)
 
 
 def string2date(string):
@@ -55,6 +54,10 @@ def string2date(string):
 
 def string2day(string):
     return datetime.strptime(string, "%Y-%m-%d").strftime("%a")
+
+
+def string2weekday(string):
+    return int(datetime.strptime(string, "%Y-%m-%d").strftime("%w"))
 
 
 def render_cell(value, max):
@@ -122,7 +125,11 @@ def main():
     )
 
     for day in sorted_days:
-        version_head += "<th>%s</th>" % string2date(day[0])
+        version_head += "<th style='background-color: %s'>%s<br>%s</th>" % (
+            oc["red"][0] if string2weekday(day[0]) in [6, 0] else oc["white"],
+            string2day(day[0]),
+            string2date(day[0]),
+        )
 
     version_head += "</tr>"
     version_body = ""
@@ -139,21 +146,30 @@ def main():
             (index - len(sorted_versions)) % len(chart_colors_text)
         ]
         if version_date != current_version_date:
-            version_body += "<tr><td style='background-color: {}' colspan='{}'></td></tr>".format(
-                empty_color, 3 + len(report[row])
+            version_body += (
+                "<tr><td style='background-color: {}' colspan='{}'></td></tr>".format(
+                    empty_color, 3 + len(report[row])
+                )
             )
         version_body += "<tr><td style='background-color: {}; color: {};'><code>{}</code></td>".format(
-            color_bg, color_text, version_datetime,
+            color_bg,
+            color_text,
+            version_datetime,
         )
 
         # older version
         if len(row) == 20:
             version_body += "<td style='background-color: {};'></td><td style='background-color: {}'></td>".format(
-                color_bg, empty_color,
+                color_bg,
+                empty_color,
             )
         else:
             version_body += "<td style='background-color: {};'><code><a style='color: {};' href='https://github.com/excalidraw/excalidraw/commit/{}'>{}</a></code></td><td style='background-color: {}'></td>".format(
-                color_bg, color_text, version_hash, version_hash, empty_color,
+                color_bg,
+                color_text,
+                version_hash,
+                version_hash,
+                empty_color,
             )
         for day in report[row]:
             version_body += render_cell(report[row][day], max_value)
